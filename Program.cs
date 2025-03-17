5b8916818f7261b9ee5a4acf690f979424c66edb
Miguel <mighuel.perassoli@gmail.com>
2025-03-17 19:47:55 -0300

    MASSA

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Tarefa Musica
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("            A DONA ARANHA             ");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(@"A Dona Aranha subiu pela parede
+Veio a chuva forte e a derrubou
+Já passou a chuva, o sol já vem surgindo
+E a Dona Aranha continua a subir");
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"Ela é teimosa e desobediente
+Sobe, sobe, sobe e nunca está contente");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(@"A Dona Aranha subiu pela parede
+Veio a chuva forte e a derrubou
+Já passou a chuva, o sol já vem surgindo
+E a Dona Aranha continua a descer");
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"Ela é teimosa e desobediente
+Sobe, sobe, sobe e nunca está contente");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(@"A Dona Aranha subiu pela parede
+Veio a chuva forte e a derrubou
+Já passou a chuva, o sol já vem surgindo
+E a Dona Aranha continua a subir");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"Ela é teimosa e desobediente
+Sobe, sobe, sobe e nunca está contente");
+
+
+
+
